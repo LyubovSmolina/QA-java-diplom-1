bb354cd21dea5fd8b2246521deecf6d70f571aad
Lyubov Smolina <turkhanova1994@yandex.ru>
2024-10-16 16:12:54 +0300

    Добавлены тесты на следующие классы: Ingredient; IngrediantType, Burger (без теста на метод getReceipt)

---
+++ src/test/java/praktikum/BurgerTest.java
@@ -0,0 +1,124 @@
+package praktikum;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.junit.Assert.assertEquals;
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class BurgerTest {
+
+    private Burger burger;
+
+    @Mock
+    private Bun mockBun;
+
+    @Mock
+    private Ingredient mockCheese;
+
+    @Mock
+    private Ingredient mockCutlet;
+
+    @Mock
+    private Ingredient mockSauce;
+
+    //Создание нового бургера
+    @Before
+    public void createNewBurger() {
+        burger = new Burger();
+   }
+
+    //Тест на проверку добавления булочек в бургер
+    @Test
+    public void setBunsTest() {
+
+        burger.setBuns(mockBun);
+        Bun actualBun = burger.bun;
+        assertEquals(mockBun, actualBun);
+
+        System.out.println("Ожидаем добавление булочки: " + mockBun);
+        System.out.println("Фактически добавлена булочка: " + actualBun);
+    }
+
+    //Тест на проверку добавления одного ингридиента
+    @Test
+    public void addOneIngredientTest() {
+
+        burger.addIngredient(mockCutlet);
+        assertEquals(1, burger.ingredients.size());
+        assertEquals(mockCutlet, burger.ingredients.get(0));
+
+        System.out.println("Ожидаем 1 ингредиент, фактически добавлено: " + burger.ingredients.size());
+        System.out.println("В бургер добавлен: " + burger.ingredients.get(0));
+
+    }
+
+    //Тест на проверку добавления двух ингридиентов
+    @Test
+    public void addTwoIngredientTest() {
+
+        burger.addIngredient(mockCutlet);
+        burger.addIngredient(mockCheese);
+        assertEquals(2, burger.ingredients.size());
+        assertEquals(mockCutlet, burger.ingredients.get(0));
+        assertEquals(mockCheese, burger.ingredients.get(1));
+
+        System.out.println("Ожидаем 2 ингредиента, фактически добавлено: " + burger.ingredients.size());
+        System.out.println("В бургер добавлены: " + burger.ingredients.get(0) +", " + burger.ingredients.get(1));
+
+    }
+
+    //Тест на удаление ингридиента
+    @Test
+    public void removeIngredientTest() {
+
+        burger.addIngredient(mockCutlet);
+        System.out.println("В бургер добавлен один ингредиент, итого ингредиентов: " + burger.ingredients.size());
+        burger.removeIngredient(0);
+        assertEquals(0, burger.ingredients.size());
+        System.out.println("Удалили ингредиент из бургера, итого ингредиентов: " + burger.ingredients.size());
+    }
+
+    //Тест на перемещение ингридиентов
+    @Test
+    public void moveIngredient() {
+
+        burger.addIngredient(mockCutlet);
+        burger.addIngredient(mockSauce);
+
+        System.out.println("Первый ингридиент: " + burger.ingredients.get(0) + ", второй ингридиент: "+ burger.ingredients.get(1));
+
+        burger.moveIngredient(0, 1);
+        assertEquals(mockSauce, burger.ingredients.get(0));
+        assertEquals(mockCutlet, burger.ingredients.get(1));
+
+        System.out.println("Изменен порядок ингридиентов, первый ингридиент: " + burger.ingredients.get(0) + ", второй ингридиент: "+ burger.ingredients.get(1));
+    }
+
+    //Тест на получение стоимости бургера, 2 булочки + 2 ингридиента. Используем мокито и явно указываем стоимость каждого ингридиента
+    @Test
+    public void getPriceTest() {
+
+        burger.setBuns(mockBun);
+        Mockito.when(mockBun.getPrice()).thenReturn(20.0f);
+        burger.addIngredient(mockCheese);
+        Mockito.when(mockCheese.getPrice()).thenReturn(25.0f);
+        burger.addIngredient(mockCutlet);
+        Mockito.when( mockCutlet.getPrice()).thenReturn(30.0f);
+
+        float expectedPrice = mockBun.getPrice() * 2 + mockCheese.getPrice() +mockCutlet.getPrice();
+        float actualPrice = burger.getPrice();
+
+        assertEquals("Неверный расчет стоимости бургера", expectedPrice, actualPrice, 0);
+
+        System.out.println("Ожидаемая стоимость бургера: "+ expectedPrice + " руб., фактическая стоимость: " + actualPrice + " руб.");
+    }
+
+//    @Test
+//    public void getReceipt() {
+//    }
+}
